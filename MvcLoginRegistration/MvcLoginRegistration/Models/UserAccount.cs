83ef48b4843bcb4ecf1ca0f38c5a91cfee719c15
Volkan <volkans66@hotmail.com>
2016-09-30 11:23:33 +0300

    Asp.net MVC5 Login Registration

---
+++ MvcLoginRegistration/MvcLoginRegistration/Models/UserAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcLoginRegistration.Models
+{
+    public class UserAccount
+    {
+
+        [Key]
+        public int UserID { get; set; }
+
+        [Required(ErrorMessage ="First name is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password",ErrorMessage ="Please confirm your password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
